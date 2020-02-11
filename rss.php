8f3f52b1a7ace1daa4ec18d459bf89dc118bf17d
yasserdawn1 <55270503+yasserdawn1@users.noreply.github.com>
2020-02-11 19:11:05 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,19 @@
+<?php
+$domOBJ = new DOMDocument();
+$domOBJ->load("");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $subject = $data->getElementsByTagName("subject")->item(0)->nodeValue;
+    $teacher = $data->getElementsByTagName("teacher")->item(0)->nodeValue;
+    $schedule = $data->getElementsByTagName("schedule")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>subject: <strong>$subject</strong></li>
+    <li>teacher: <strong>$teacher</strong></li>
+    <li>schedule: <strong>$schedule</strong></li>
+    </ul>";
+}
